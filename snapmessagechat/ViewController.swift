38fc3053f492c742de40b94f42b21e36209f1092
mmanion <mmanion10@gmail.com>
2019-08-22 18:18:44 -0400

    first commit

---
+++ snapmessagechat/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  snapmessagechat
+//
+//  Created by Matthew Manion on 8/22/19.
+//  Copyright © 2019 Matthew Manion. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
