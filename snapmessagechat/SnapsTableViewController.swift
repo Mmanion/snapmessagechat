bfa3499362b10400962930ab253970b7c156d5d7
mmanion <mmanion10@gmail.com>
2019-08-25 14:55:36 -0400

    Added ViewSnapsViewController

--- snapmessagechat/SnapsTableViewController.swift
+++ snapmessagechat/SnapsTableViewController.swift
@@ -8,14 +8,40 @@
 //  <div>Icons made by <a href="https://www.flaticon.com/authors/smashicons" title="Smashicons">Smashicons</a> from <a href="https://www.flaticon.com/"             title="Flaticon">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/"             title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a></div>
 
 import UIKit
-import Firebase 
+import Firebase
+import FirebaseDatabase
 
 class SnapsTableViewController: UITableViewController {
+    
+    var snaps : [DataSnapshot] = []
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        if let uid = Auth.auth().currentUser?.uid {
 
-
+        Database.database().reference().child("users").child(uid).child("snaps").observe(.childAdded) { (snapshot) in
+            
+                
+                self.snaps.append(snapshot)
+                self.tableView.reloadData()
+                
+            
+             if let snapDictionary = snapshot.value as? NSDictionary {
+                if let from = snapDictionary["from"] as? String {
+                    if let imageName = snapDictionary["imageName"] as? String {
+                        if let imageURL = snapDictionary["imageURL"] as? String {
+                            if let message = snapDictionary["message"] as? String {
+                                
+                            }
+                        }
+                    }
+                }
+             }
+            
+            }
+        
+        }
         
     }
     
@@ -26,14 +52,28 @@ class SnapsTableViewController: UITableViewController {
 
     // MARK: - Table view data source
 
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
-    }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return 0
+        return snaps.count
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        performSegue(withIdentifier: "snapsToView", sender: nil)
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier2", for: indexPath)
+        
+        let snap = snaps[indexPath.row]
+        
+        if let snapDictionary = snap.value as? NSDictionary {
+            if let from = snapDictionary["from"] as? String {
+                cell.textLabel?.text = from
+            }
+        }
+        
+        return cell
     }
 
     
---
+++ snapmessagechat/ViewSnapViewController.swift
@@ -0,0 +1,34 @@
+//
+//  ViewSnapViewController.swift
+//  snapmessagechat
+//
+//  Created by Matthew Manion on 8/25/19.
+//  Copyright © 2019 Matthew Manion. All rights reserved.
+//
+
+import UIKit
+
+class ViewSnapViewController: UIViewController {
+    
+    @IBOutlet weak var imageView: UIImageView!
+    
+    @IBOutlet weak var messageLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
